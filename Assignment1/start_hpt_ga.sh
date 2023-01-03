5437ceeb725dfbedcca5a02a0fdc6b32a02035c1
alexl4123 <alexl.id.at@gmail.com>
2023-01-03 11:52:15 +0100

    [Prg-Ex2:2/3] - Backup push, as ssh connection was recently lost

--- Assignment1/start_hpt_ga.sh
+++ Assignment1/start_hpt_ga.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qsub -N ga-hpt -l h_vmem=2G -r y -pe pthreads 11 -e ~/ga_00.txt -o ~/ga_01.txt ~/HOTWS22/Assignment1/hpt_ga.sh
+qsub -N m-ga-hpt -l h_vmem=2G -r y -pe pthreads 11 -e ~/m_ga_00.txt -o ~/m_ga_01.txt ~/HOTWS22/Assignment1/hpt_ga.sh
